fa61a3e0ecbcdbc81e0e1d2ddb1c447e0037ab35
RémiSaurel <r.saurel5@gmail.com>
2023-02-27 19:22:26 +0100

    add: pass block and lockios on props

--- src/components/lockios/LockiosBlock.vue
+++ src/components/lockios/LockiosBlock.vue
@@ -2,17 +2,18 @@
   <div class="lockio-block flex flex-col md:flex-row mb-12 mr-8">
     <div class="md:w-1/2 md:mr-8">
       <div class="text-2xl font-bold">
-        {{ props.title }}
+        <span v-if="props.block.status === 'AVAILABLE'"> 🟢 </span>
+        <span v-else> 🔴 </span>
+        {{ title }}
       </div>
-      <!-- SIMULATE THE LOCKERS WITH RECTANGLES AND 3*3 BLOCK -->
       <div class="mt-6">
-        <locker-grid></locker-grid>
+        <locker-grid :lockios="props.lockios"></locker-grid>
       </div>
     </div>
     <div class="mt-8 md:w-1/2 md:mt-0">
       <div class="text-2xl font-bold">Infos utiles</div>
       <div class="mt-2 md:mt-6">
-        <useful-infos></useful-infos>
+        <useful-infos :lockios="props.lockios"></useful-infos>
       </div>
     </div>
   </div>
@@ -21,10 +22,12 @@
 <script setup lang="ts">
 import LockerGrid from "./LockerGrid.vue";
 import UsefulInfos from "./UsefulInfos.vue";
+import { Block, Lockio } from "../../models/models";
 
 const props = defineProps<{
-  title: string;
+  block: Block;
+  lockios: Lockio[];
 }>();
 
-const title = props.title;
+const title = props.block.name;
 </script>
