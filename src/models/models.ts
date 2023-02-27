5bfa45b5fe0e9f9e7082280078455186711e6ba6
RémiSaurel <r.saurel5@gmail.com>
2023-02-27 19:14:17 +0100

    add: lockio and block interface

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,4 +1,17 @@
-export interface Locker {
+export interface Lockio {
   id: number;
-  status: "available" | "unavailable";
+  blockId: number;
+  localId: number;
+  size: "SMALL" | "MEDIUM" | "LARGE";
+  status: "AVAILABLE" | "OCCUPIED" | "DISABLED" | "PRERESERVED";
+}
+
+export interface Block {
+  id: number;
+  name: string;
+  coordinate: string;
+  status: "AVAILABLE" | "DISABLED";
+  privacy: "PUBLIC" | "PRIVATE" | "MIXED";
+  lockios: Lockio[];
+  url: string;
 }
