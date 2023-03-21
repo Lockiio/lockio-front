7248cd3d9752d48ccfa126b40a3c39c3b0e63213
RémiSaurel <r.saurel5@gmail.com>
2023-03-21 11:35:40 +0100

    add: real text and questions

--- src/components/LandingPageRow.vue
+++ src/components/LandingPageRow.vue
@@ -4,21 +4,28 @@
       <img src="/src/assets/reserver.svg" alt="logo" class="h-48 w-48 m-auto"/>
       <div class="font-bold">Réserver votre Lockio</div>
       <div class="mb-8">
-        {{ lorem }}
+        Vous pouvez directement consulter les disponibilités et réserver votre Lockio en ligne.
+        Il ne vous restera plus qu'à venir sur place, utiliser votre code fournie par nos soins
+        et profiter de votre Lockio ! Pour plus d'informations, n'hésitez pas à consulter la Foire
+        aux questions ou à nous contacter !
       </div>
     </div>
     <div class="w-auto md:w-1/4">
       <img src="/src/assets/zen.svg" alt="logo" class="h-48 w-48 m-auto"/>
       <div class="font-bold">Partir Zen</div>
       <div class="mb-8">
-        {{ lorem }}
+        Lockio met un point d'honneur sur la sécurité de vos biens. Nos Lockios sont équipés de
+        serrures de haute sécurité et sont constamment surveillés par des caméras de surveillance.
+        Vous pouvez laisser vos affaires en toute confiance.
       </div>
     </div>
     <div class="w-auto md:w-1/4">
       <img src="/src/assets/contribuer.svg" alt="logo" class="h-48 w-48 m-auto"/>
       <div class="font-bold">Contribuer</div>
       <div class="mb-8">
-        {{ lorem }}
+        L'environnement est un sujet qui nous tient à coeur. Lockio est une entreprise éco-responsable
+        qui s'engage à réduire son impact sur l'environnement. Nos services permettront par exemple d'augmenter
+        l'utilisation des transports en commun, du covoiturage et de la location de vélos.
       </div>
     </div>
   </div>
--- src/components/MainQuestions.vue
+++ src/components/MainQuestions.vue
@@ -2,8 +2,8 @@
   <div class="text-3xl font-bold text-center">Foire aux questions</div>
   <div v-for="question in questions" class="max-w-2xl m-auto">
     <QuestionAccordion
-      :question="question.question"
-      :answer="question.answer"
+        :question="question.question"
+        :answer="question.answer"
     ></QuestionAccordion>
   </div>
 </template>
@@ -14,19 +14,21 @@ import QuestionAccordion from "./QuestionAccordion.vue";
 const questions = [
   {
     question: "Comment puis-je utiliser un Lockio ?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lorem dolor, tristique vitae condimentum sed, porta efficitur ipsum. Suspendisse potenti. Quisque ultricies viverra ex, ac elementum augue porttitor quis. Vestibulum eleifend sodales leo, vitae molestie augue blandit nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lorem dolor, tristique vitae condimentum sed, porta efficitur ipsum. Suspendisse potenti. Quisque ultricies viverra ex, ac elementum augue porttitor quis. Vestibulum eleifend sodales leo, vitae molestie augue blandit nec. ",
+    answer: "L'utilisation d'un Lockio est très simple. Les indications seront sur l'écran de la borne." +
+        "Pour déposer vos affaires, sélectionnez un des casiers disponibles. Une fois votre choix effectué, un code vous " +
+        "sera fourni et vous permettra de déverrouiller le Lockio. La porte s'ouvrira alors automatiquement et vos affaires pourront être récupérés."
   },
   {
     question: "Comment le système de réservation fonctionne-t-il ?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lorem dolor, tristique vitae condimentum sed, porta efficitur ipsum. Suspendisse potenti. Quisque ultricies viverra ex, ac elementum augue porttitor quis. Vestibulum eleifend sodales leo, vitae molestie augue blandit nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lorem dolor, tristique vitae condimentum sed, porta efficitur ipsum. Suspendisse potenti. Quisque ultricies viverra ex, ac elementum augue porttitor quis. Vestibulum eleifend sodales leo, vitae molestie augue blandit nec. ",
+    answer: "Le système de réservation vous permet d'anticiper votre arrivée sur place. Sur notre site, vous pouvez 'réserver' un Lockio disponible. " +
+        "Lorsque le Lockio est réservé, un code vous est fourni. Vous disposez alors de 30min pour venir sur place et indiquer, pour le Lockio réservé le code fourni par nos soins. " +
+        "Si vous n'avez pas déposé vos affaires dans les 30min, le Lockio sera libéré et pourra être utilisé par un autre client."
   },
   {
     question:
-      "Cette question est très cool, mais je ne sais pas vraiment comment la formuler (c’est un bon exemple de question longue) ?",
+        "Y-a-t-il une durée maximum d'utilisation ?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lorem dolor, tristique vitae condimentum sed, porta efficitur ipsum. Suspendisse potenti. Quisque ultricies viverra ex, ac elementum augue porttitor quis. Vestibulum eleifend sodales leo, vitae molestie augue blandit nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lorem dolor, tristique vitae condimentum sed, porta efficitur ipsum. Suspendisse potenti. Quisque ultricies viverra ex, ac elementum augue porttitor quis. Vestibulum eleifend sodales leo, vitae molestie augue blandit nec. ",
+        "Oui. Pour des raisons de sécurité et de disponibilité, le temps d'utilisation d'un Lockio est limité à 72h, soit 3 jours. "
   },
 ];
 </script>
