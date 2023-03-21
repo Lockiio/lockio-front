117c3a5b59a1c0029e9fc1dbe2143baa98059707
RémiSaurel <r.saurel5@gmail.com>
2023-03-21 12:13:22 +0100

    add: some styling

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="container">
     <NavBar></NavBar>
+    <img class="absolute -left-80 top-24 w-3/5 h-1/3" src="/src/assets/blur.svg" alt="logo"/>
     <div class="flex-col text-center">
       <TitleSubtitle></TitleSubtitle>
       <div class="mt-8">
         <button
-          class="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
-          @click="$router.push({ name: 'Login' })"
+            class="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
+            @click="$router.push({ name: 'Login' })"
         >
           Réserver un Lockio
-          <font-awesome-icon icon="fa-solid fa-chevron-right" />
+          <font-awesome-icon icon="fa-solid fa-chevron-right"/>
         </button>
       </div>
       <div class="mt-16 text-3xl font-extrabold">Lockio, c'est quoi ?</div>
@@ -19,6 +20,7 @@
     </div>
     <div class="mt-8">
       <MainQuestions></MainQuestions>
+      <img class="absolute -right-60 bottom-2 w-1/2 h-1/5 -z-50" src="/src/assets/blur.svg" alt="logo"/>
     </div>
   </div>
 </template>
@@ -26,7 +28,7 @@
 <script setup>
 import NavBar from "../components/navbars/NavBar.vue";
 import TitleSubtitle from "../components/TitleSubtitle.vue";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import LandingPageRow from "../components/LandingPageRow.vue";
 import MainQuestions from "../components/MainQuestions.vue";
 </script>
